7dcdb52723645cdc896967d49ea9944db1e0939f
Baku Wall-e <103138773+Baku-Stark@users.noreply.github.com>
2022-07-07 11:07:19 -0300

    Update Aula 10 - Repetições (Parte 1).js

--- Aulas de JavaScript/Aula 10 - Repetições (Parte 1).js
+++ Aulas de JavaScript/Aula 10 - Repetições (Parte 1).js
@@ -1,5 +1,14 @@
+{USANDO APENAS A FUNÇÃO "while"}
 var c = 0
 while (c < 6) {
     c++
     console.log(`${c}° - Tudo bem?`)
 }
+
+
+{USANDO O "do"}
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
