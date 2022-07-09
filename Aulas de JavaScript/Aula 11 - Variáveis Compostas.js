517cbcf88ee2898b7584fba10d6fa9b1c7439a58
Baku Wall-e <103138773+Baku-Stark@users.noreply.github.com>
2022-07-09 14:30:46 -0300

    Update Aula 11 - Variáveis Compostas.js

--- Aulas de JavaScript/Aula 11 - Variáveis Compostas.js
+++ Aulas de JavaScript/Aula 11 - Variáveis Compostas.js
@@ -17,3 +17,14 @@ num.sort()
 console.log('---> Números do vetor em ordem crescente: ' + num)
 console.log(`O vetor possui: ${num.length} casa(s)`)
 console.log(`O primeiro número é o ${num[0]}`)
+
+let valores = [8, 1, 7, 4, 2, 9]
+/* console.log(valores[0])
+console.log(valores[1])
+console.log(valores[2])
+console.log(valores[3])
+console.log(valores[4])
+console.log(valores[5]) */
+for(let pos = 0; pos < valores.length; pos++) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
