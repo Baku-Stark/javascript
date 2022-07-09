1b8acac6adb45abb8463b9c721d7de37a28499eb
Baku Wall-e <103138773+Baku-Stark@users.noreply.github.com>
2022-07-09 16:12:52 -0300

    Update Aula 12 - Funções.js

--- Aulas de JavaScript/Aula 12 - Funções.js
+++ Aulas de JavaScript/Aula 12 - Funções.js
@@ -13,3 +13,45 @@ console.log(res)
 
 // Outro método de imprimir
 console.log(parimpar(5))
+
+
+{Adição}
+function soma(n1, n2) {
+    return n1 + n2
+}
+
+console.log(soma(2, 5))
+
+function soma(n1=0, n2=0) {
+    return n1 + n2
+}
+
+console.log(soma(2, 5))
+
+
+{Multiplicação}
+let v = function(x) {
+    return x * 2
+}
+
+console.log(v(5))
+
+
+{Fatorial}
+function fatorial(n) {
+    let fat = 1
+    for(let c = n; c > 1; c--) {
+        fat *= c
+    }
+    return fat
+}
+console.log(fatorial(5))
+
+function fatorial(n) {
+    if (n ==1) {
+        return 1
+    } else {
+        return n * fatorial(n-1)
+    }
+}
+console.log(fatorial(5))
