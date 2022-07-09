e97548673e08cfd0c63d129fceab4f0c83c59693
Baku Wall-e <103138773+Baku-Stark@users.noreply.github.com>
2022-07-09 15:36:31 -0300

    Update Aula 11 - Variáveis Compostas.js

--- Aulas de JavaScript/Aula 11 - Variáveis Compostas.js
+++ Aulas de JavaScript/Aula 11 - Variáveis Compostas.js
@@ -7,8 +7,10 @@ console.log(`Nosso vetor é o [${num}]`)
 num.length  // Contagem de casas no vetor
 console.log(`Nosso vetor é o [${num}] e possui [${num.length} casas]`)
 
+
 let num = [5, 8, 4] //Contagem crescente "sort()"
 console.log(`A ordem crescente é: ${num.sort()}`)
+
 
 {EXEMPLO}
 let num = [5, 8, 2, 9, 3]
@@ -18,6 +20,7 @@ console.log('---> Números do vetor em ordem crescente: ' + num)
 console.log(`O vetor possui: ${num.length} casa(s)`)
 console.log(`O primeiro número é o ${num[0]}`)
 
+
 let num = [5, 8, 2, 9, 3]
 num.push(1)
 num.sort()
@@ -25,6 +28,21 @@ console.log('---> Números do vetor em ordem crescente: ' + num)
 console.log(`O vetor possui: ${num.length} casa(s)`)
 console.log(`O primeiro número é o ${num[0]}`)
 console.log(`O número 8 está na posição: ${num.indexOf(8)}`) //Encontrar números desejados
+
+
+let num = [5, 8, 2, 9, 3]
+num.push(1)
+num.sort()
+console.log('---> Números do vetor em ordem crescente: ' + num)
+console.log(`O vetor possui: ${num.length} casa(s)`)
+console.log(`O primeiro número é o ${num[0]}`)
+let pos = num.indexOf(8)
+if (pos == -1) {
+    console.log('>>> O valor não pode ser encontrado')
+} else {
+    console.log(`>>> O número 8 está na posição: ${pos}`)
+}
+
 
 let valores = [8, 1, 7, 4, 2, 9]
 /* console.log(valores[0])
