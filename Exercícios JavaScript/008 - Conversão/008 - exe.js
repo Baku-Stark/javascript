3b5151e0d4b18f72f84266d808386845724579a0
Baku Wall-e <103138773+Baku-Stark@users.noreply.github.com>
2022-10-07 10:33:10 -0300

    Add files via upload

---
+++ Exercícios JavaScript/008 - Conversão/008 - exe.js
@@ -0,0 +1,7 @@
+var priSelect = document.querySelector('.pri-select')
+var secSelect = document.querySelector('.sec-select')
+
+var real = document.querySelector('#real')
+var dolar = document.querySelector('#dolar')
+
+console.log(`${priSelect} - ${secSelect}`)
