06478d0c060089ebc593fd980203c2a93a90e041
Baku Wall-e <wysthellyjessemata13@gmail.com>
2022-10-11 14:51:36 -0300

    update

--- Exercícios JavaScript/008 - Conversão/008 - exe.js
+++ Exercícios JavaScript/008 - Conversão/008 - exe.js
@@ -1,7 +1,31 @@
-var priSelect = document.querySelector('.pri-select')
-var secSelect = document.querySelector('.sec-select')
+function Convert(){
+    //-------------- SELECIONAR ELEMENTO
+    var priSelect = document.querySelector('.pri-select')
+    var secSelect = document.querySelector('.sec-select')
+    
+    //-------------- ALTERAR TEXTO
+    var select = document.getElementById("selec-1")
+    var select2 = document.getElementById("selec-2")
+    var valor1 = select.options[select.selectedIndex].text
+    var valor2 = select.options[select2.selectedIndex].text
 
-var real = document.querySelector('#real')
-var dolar = document.querySelector('#dolar')
+    if(valor1 == valor2){
+        window.alert("Você precisa escolher outro valor de moeda para conversão.")
+    }
+    else{
+        priSelect.innerHTML = `<strong>${valor1}</strong>`
+        secSelect.innerHTML = `<strong>${valor2}</strong>`
+    }
 
-console.log(`${priSelect} - ${secSelect}`)
+    //-------------- SELECIONAR VALOR PARA CALCULADO [moeda]
+    var set = document.querySelector('#num')
+    var moeda = parseInt(set.value)
+    var res = document.querySelector('.value-box')
+
+    // DÓLAR PARA REAL === x5.2061
+    if(valor1 == "Dólar" && valor2 == "Real"){
+        var result = moeda * 5.2061
+        res.innerHTML = `<p><strong>U$${result}</strong></p>`
+    }
+
+}
