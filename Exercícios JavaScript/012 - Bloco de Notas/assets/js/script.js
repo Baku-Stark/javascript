fa4e7b31e295164f9c6773ebaf096e61d0028350
Baku Stark <103138773+Baku-Stark@users.noreply.github.com>
2023-01-24 02:52:20 -0300

    Update script.js

--- Exercícios JavaScript/012 - Bloco de Notas/assets/js/script.js
+++ Exercícios JavaScript/012 - Bloco de Notas/assets/js/script.js
@@ -1,39 +1,96 @@
+// ├── createBTN -> Seletor do <button> do formulário
+// ├── tbodyTable -> Seletor do <tbody> da tabela
 const createBTN = document.querySelector(".main_btn")
+const tbodyTable = document.querySelector('.body-table')
 
-function create_Note(task, note){
-    // Salvando dados no localStorage
-    localStorage.setItem(task, JSON.stringify(note))
+function create_Note(task_id, note){
+    /*
+        ├── Salvando dados no localStorage.
+        ├
+        ├── Args:
+            ├── task_id -> ID da lista;
+            └── note -> Anotação do usuário.
+    */
+    localStorage.setItem(task_id, JSON.stringify(note))
 }
 
-function delete_Note(noteName){
+function delete_Note(task_id){
     /*
-        ├── Ler o contéudo do localStorage
+        ├── Deletando dados do localStorage.
         ├
-        ├── Return:
-            └── noteName ->  Nome do dicionário salvo
+        ├── Args:
+            └── task_id -> ID da anotação.
     */
-    // Deletando dados do localStorage
+    localStorage.remove(task_id)
 }
 
 function read_Note(){
     /*
-        ├── Ler o contéudo do localStorage
+        ├── Ler o contéudo do localStorage.
     */
+    console.log('Função ativada!!!')
 }
+
+const newRoleTable = (task_id, note, qtd_Select) => {
+    /*
+        ├── Criar uma nova linha na tabela.
+        ├
+        ├── Args:
+            ├── task_id -> ID selecionado pelo usuário;
+            ├── note -> Anotação principal
+            └── qtd_Select -> Quantidade
+        ├
+        ├── Return:
+            ├── tbodyTable -> Seletor do <tbody> da tabela
+            ├── content_TR -> Criar um elemento <tr>
+                ├── content_TD_id -> ID da anotação
+    */
+
+    const content_TR = document.createElement('tr')
+    
+    // ├── ID
+    const content_TD_id = document.createElement('td')
+    content_TD.textContent = task_id
+
+    // ├── Note
+    const content_TD_note = document.createElement('td')
+
+    content_TD_note.textContent = note
+    // ├── QTD
+    const content_TD_qtd = document.createElement('td')
+    content_TD_qtd.textContent = qtd_Select
+
+    // ├── Button
+    const content_TD_button = document.createElement('td')
+    const delete_button = document.createElement('button')
+    const icon_trash = document.createElement('i')
+    icon_trash.classList.add('bi')
+    icon_trash.classList.add('bi-trash-fill')
+    delete_button.textContent = icon_trash
+    content_TD_button.textContent = delete_button
+
+    content_TR.appendChild(content_TD_id)
+    content_TR.appendChild(content_TD_note)
+    content_TR.appendChild(content_TD_qtd)
+    content_TR.appendChild(content_TD_button)
+
+    tbodyTable.appendChild(content_TR)
+}
+
 
 createBTN.addEventListener('click', (e) => {
     /*
-        ├── Função para ativar o CRUD.
+        ├── Função para adicionar conteúdo ao localStorage.
         ├
         ├── Return:
             ├── preventDefault() -> Impedir página de recarregar.
             ├── con_Select -> Valor da <textarea> (str)
-            ├── task_Select -> Valor do <input type="text"> (str)
+            ├── taskID_Select -> Valor do <input type="text"> (str)
             └── qtd_Select -> Valor do <input type="number"> (int)
-    */ 
+    */
     e.preventDefault()
     let con_Select = document.getElementById('i_conteudo').value
-    let task_Select = document.querySelector('#i_obs').value
+    let taskID_Select = document.querySelector('#i_taskID').value
     let qtd_Select = document.querySelector('#i_qtd')
 
     if(con_Select === ""){
@@ -57,6 +114,7 @@ createBTN.addEventListener('click', (e) => {
             }
 
             create_Note(noteContent_Dic)
+            newRoleTable(taskID_Select, con_Select, qtd_Select)
         }
 
         else{
@@ -67,11 +125,15 @@ createBTN.addEventListener('click', (e) => {
                 "quantidade": qtd_Select
             }
 
-            create_Note(task_Select, noteContent_Dic)
+            create_Note(taskID_Select, noteContent_Dic)
             console.log('\033[1;32m [-][VALID] Inserido ao localStorage com sucesso! \033[m')
+            newRoleTable(taskID_Select, con_Select, qtd_Select)
 
             // Alterar texto no botão
             createBTN.textContent = "Fazer anotação"
         }
     }
 })
+
+// Ler notas do localStorage.
+read_Note()
