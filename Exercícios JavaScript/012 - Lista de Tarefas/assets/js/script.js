aa21c65f014f9cb071f5de0a18b20133dae696e8
Baku Wall-e <wysthellyjessemata13@gmail.com>
2023-01-24 16:01:30 -0300

    💿 Update: ToDo List

--- Exercícios JavaScript/012 - Lista de Tarefas/assets/js/script.js
+++ Exercícios JavaScript/012 - Lista de Tarefas/assets/js/script.js
@@ -12,6 +12,7 @@ function create_Note(task_id, note){
             └── note -> Anotação do usuário.
     */
     localStorage.setItem(task_id, JSON.stringify(note))
+    console.log('\033[1;32m [-][VALID] Inserido ao localStorage com sucesso! \033[m')
 }
 
 function delete_Note(task_id){
@@ -20,15 +21,25 @@ function delete_Note(task_id){
         ├
         ├── Args:
             └── task_id -> ID da anotação.
+        ├
+        ├── Return:
+            └── tr_line = Seletor da <tr> com o seu ID.
     */
-    localStorage.remove(task_id)
+   localStorage.removeItem(task_id)
+   
+   const tr_line = document.querySelector(`.tr_line${task_id}`)
+   document.querySelector('.body-table').removeChild(tr_line)
+
+   console.log('\033[1;35m [-][DELETE] Task deletada com sucesso! \033[m')
+   console.log('\033[1;35m'+`    └── [DEL] -> [${task_id}]`+'\033[m')
 }
 
-function read_Note(){
+function read_Note(task_id){
     /*
         ├── Ler o contéudo do localStorage.
     */
-    console.log('Função ativada!!!')
+    console.log('\033[1;34m [-][CHECK] Leitura do localStorage feita com sucesso! \033[m')
+    console.log('\033[1;34m'+`    └── [CREATED] -> [${task_id}]`+'\033[m')
 }
 
 const newRoleTable = (task_id, note, qtd_Select) => {
@@ -43,38 +54,62 @@ const newRoleTable = (task_id, note, qtd_Select) => {
         ├── Return:
             ├── tbodyTable -> Seletor do <tbody> da tabela
             ├── content_TR -> Criar um elemento <tr>
-                ├── content_TD_id -> ID da anotação
+                ├── content_TD_id -> Linhda do 'ID'
+                ├── content_TD_note -> Linha de 'Anotção'
+                ├── content_TD_qtd -> Linha de 'Quantidade'
+                └── content_TD_button -> Linha do 'Botão'
+                    ├── delete_button -> Botão
+                    └── icon_trash -> Ícone de lixeira
     */
 
     const content_TR = document.createElement('tr')
+    content_TR.classList.add(`tr_line${task_id}`)
     
     // ├── ID
     const content_TD_id = document.createElement('td')
-    content_TD.textContent = task_id
+    content_TD_id.textContent = task_id
+    content_TR.appendChild(content_TD_id)
 
     // ├── Note
     const content_TD_note = document.createElement('td')
-
     content_TD_note.textContent = note
+    content_TR.appendChild(content_TD_note)
+    var click = 0
+
+    content_TD_note.addEventListener('click', () => {
+        if(click === 0){
+            content_TD_note.style.color = "#32D94B"
+            content_TD_note.style.textDecoration = "none"
+            click = 1
+        }
+        else{
+            content_TD_note.style.color = "#4D60EB"
+            content_TD_note.style.textDecoration = "line-through"
+            click = 0
+        }
+    })
+
     // ├── QTD
     const content_TD_qtd = document.createElement('td')
     content_TD_qtd.textContent = qtd_Select
+    content_TR.appendChild(content_TD_qtd)
 
     // ├── Button
     const content_TD_button = document.createElement('td')
+
     const delete_button = document.createElement('button')
     const icon_trash = document.createElement('i')
     icon_trash.classList.add('bi')
     icon_trash.classList.add('bi-trash-fill')
-    delete_button.textContent = icon_trash
-    content_TD_button.textContent = delete_button
-
-    content_TR.appendChild(content_TD_id)
-    content_TR.appendChild(content_TD_note)
-    content_TR.appendChild(content_TD_qtd)
     content_TR.appendChild(content_TD_button)
+    content_TD_button.appendChild(delete_button)
+    delete_button.appendChild(icon_trash)
 
     tbodyTable.appendChild(content_TR)
+
+    delete_button.addEventListener('click', () => {
+        delete_Note(task_id)
+    })
 }
 
 
@@ -92,6 +127,8 @@ createBTN.addEventListener('click', (e) => {
     let con_Select = document.getElementById('i_conteudo').value
     let taskID_Select = document.querySelector('#i_taskID').value
     let qtd_Select = document.querySelector('#i_qtd')
+
+    taskID_Select = taskID_Select.toLowerCase()
 
     if(con_Select === ""){
         window.alert("Preencha o campo de anotação.")
@@ -114,6 +151,7 @@ createBTN.addEventListener('click', (e) => {
             }
 
             create_Note(noteContent_Dic)
+            read_Note(taskID_Select)
             newRoleTable(taskID_Select, con_Select, qtd_Select)
         }
 
@@ -126,7 +164,7 @@ createBTN.addEventListener('click', (e) => {
             }
 
             create_Note(taskID_Select, noteContent_Dic)
-            console.log('\033[1;32m [-][VALID] Inserido ao localStorage com sucesso! \033[m')
+            read_Note(taskID_Select)
             newRoleTable(taskID_Select, con_Select, qtd_Select)
 
             // Alterar texto no botão
@@ -134,6 +172,3 @@ createBTN.addEventListener('click', (e) => {
         }
     }
 })
-
-// Ler notas do localStorage.
-read_Note()
