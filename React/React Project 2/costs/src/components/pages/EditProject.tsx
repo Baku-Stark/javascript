f4fbd4ff3910d9b285c0a47b7737ecacc3cb1c06
Baku Wall-e <wysthellyjessemata13@gmail.com>
2023-07-10 17:14:33 -0300

    ⚛️ Update: `Costs Project`

--- React/React Project 2/costs/src/components/pages/EditProject.tsx
+++ React/React Project 2/costs/src/components/pages/EditProject.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
 import { Loading } from '../layouts/Loading'
-import { LinkButton } from '../layouts/LinkButton'
+import { ProjectForm } from '../../content/ProjectForm'
 import styles from '../../assets/css/EditProject.module.css'
 
 export function EditProject(){
@@ -12,6 +12,12 @@ export function EditProject(){
     const [showProjectForm, setShowProjectForm] = useState(false)
     function toggleProjectForm(){
         setShowProjectForm(!showProjectForm)
+        document.title = `${project.name} - Edit`
+    }
+
+    const [showServiceForm, setShowServiceForm] = useState(false)
+    function toggleServiceForm(){
+        setShowServiceForm(!showServiceForm)
     }
 
     const [project, setProject] = useState([])
@@ -33,45 +39,98 @@ export function EditProject(){
         document.title = `${project.name} - Edit`
     }, [id])
 
+    // === EDITAR INFORMAÇÕES
+    async function updateProject(project: any){
+        if(project['number'] < project['cost']){
+            console.log("MAIOR!!")
+        }
+
+        else{
+            await fetch(`http://localhost:5000/projects/${project['id']}`, {
+                method:'PATCH',
+                body:JSON.stringify(project),
+                headers:{
+                    'Content-Type':'application/json'
+                }
+            })
+                .then((resp) => resp.json())
+                .then((data) => {
+                    setProject(data)
+                    setShowProjectForm(false)
+                })
+                .catch((err) => console.log(err))
+        }
+    }
+
     return(
         <>
-            {project.name ? (
-                <div className={styles.edit_container}>
-                    <div className={styles.title_container}>
-                        <h2>
-                            Projeto: <span>{project['name']}</span>
-                        </h2>
-                        <button
-                            onClick={toggleProjectForm}
-                            className={styles.btn}
-                        >
-                            {!showProjectForm ? "Editar Projeto" : "Fechar Formulario"}
-                        </button>
+            {project.name ? 
+            (
+                <>
+                    {/* ===== PROJECTS ===== */}
+                    <div className={styles.edit_container}>
+                        <div className={styles.title_container}>
+                            <h2>
+                                Projeto: <span>{project['name']}</span>
+                            </h2>
+                            <button
+                                onClick={toggleProjectForm}
+                                className={styles.btn}
+                            >
+                                {!showProjectForm ? "Editar Projeto" : "Fechar Formulario"}
+                            </button>
+                        </div>
+                        {!showProjectForm ? (
+                            <div className={styles.content_container}>
+                                <h4>Detalhes do projeto</h4>
+                                <p>
+                                    <span>Categoria: </span>{project['category']['name']}
+                                </p>
+                                <p>
+                                    <span>Orçamento: </span>R${project['number']}
+                                </p>
+                                <p>
+                                    <span>Total Utilizado: </span>R${project['cost']}
+                                </p>
+                            </div>
+                        ) : (
+                            <ProjectForm handleSubmit={updateProject} btnText="Concluir Edição" projectData={project} />
+                        )}
                     </div>
-                    {!showProjectForm ? (
+                    {/* ===== SERVICES ===== */}
+                    <div className={styles.edit_container}>
+                        <div className={styles.title_container}>
+                            <h2>
+                               Adicione um serviço
+                            </h2>
+                            <button
+                                onClick={toggleServiceForm}
+                                className={styles.btn}
+                            >
+                                {!showServiceForm ? "Adicionar" : "Fechar Formulario"}
+                            </button>
+                        </div>
+                        {!showServiceForm ? (
+                            <div className={styles.content_container}>
+                                <h4>Serviços</h4>
+                            </div>
+                        ) : (
+                            <div>FOMULARIO</div>
+                        ) }
+                    </div>
+                    <div className={styles.service_container}>
+                        <h2>
+                            Adicione um serviço
+                        </h2>
                         <div className={styles.content_container}>
-                            <h4>Detalhes do projeto</h4>
                             <p>
-                                <span>Categoria: </span>{project['category']['name']}
-                            </p>
-                            <p>
-                                <span>Orçamento: </span>R${project['number']}
-                            </p>
-                            <p>
-                                <span>Total Utilizado: </span>R${project['number']}
+                                Itens de serviço
                             </p>
                         </div>
-                    ): (
-                        <form method='POST'>
-                            <fieldset>
-                                <legend>Editando</legend>
-                                <div></div>
-                            </fieldset>
-                        </form>
-                    )}
-                </div>
+                    </div>
+                </>
             ) : (
-            <Loading/>
+                <Loading/>
             )}
         </>
     )
