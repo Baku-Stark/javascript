afae0d25d9bb9399adad1d05001a034b3dfb9309
Baku Wall-e <wysthellyjessemata13@gmail.com>
2023-08-18 17:43:02 -0300

    💿 Update: `Node Course`

--- Node_js/Curso/modules/express.js
+++ Node_js/Curso/modules/express.js
@@ -5,28 +5,51 @@ app.use(express.json());
 
 const UserModel = require('../src/models/user.model');
 
-app.get('/home', (req, res) => {
-    res.contentType("application/html");
-    res.status(200).send("<h1>Hello World!</h1>");
+app.get('/users', async (req, res) => {
+    try{
+        const users = await UserModel.find({});
+        res.status(200).json(users);
+    }
+    catch(error){
+        return res.status(500).send(error.message);
+    }
 });
 
-app.get('/users', (req, res) => {
-    const users = [
-        {
-            "name": "Jonh Doe",
-            "email": "john@doe.com"
-        },
-        {
-            "name": "Jane Doe",
-            "email": "jane@doe.com"
-        },
-        {
-            "name": "Wallace (Express)",
-            "email": "wallace@doe.com"
-        },
-    ]
-    res.contentType("application/json");
-    res.status(200).json(users);
+app.get('/users/:id', async (req, res) => {
+    try{
+        const id = req.params.id;
+        const user = await UserModel.findById(id);
+        return res.status(200).json(user);
+    }
+    catch(error){
+        return res.status(500).send(error.message);
+    }
+});
+
+app.delete('/users/:id', async (req, res) => {
+    try{
+        const id = req.params.id;
+        const user = await UserModel.findByIdAndRemove(id);
+        return res.status(200).json(user);
+    }
+    catch(error){
+        return res.status(500).send(error.message);
+    }
+});
+
+app.put('/users/:id', async (req, res) => {
+    try{
+        const id = req.params.id;
+        const user = await UserModel.findByIdAndUpdate(
+            id,
+            req.body,
+            {new: true}
+        );
+        return res.status(200).json(user);
+    }
+    catch(error){
+        return res.status(500).send(error.message);
+    }
 });
 
 app.post('/users', async (req, res) => {
